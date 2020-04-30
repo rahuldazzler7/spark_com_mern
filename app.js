cd95cf2ef4c5b7a34f2d9cb51c56e10788695668
Rahul Sarkar <rahulsarkar334@gmail.com>
2020-04-30 22:58:57 +0530

    package changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const dotend = require("dotenv").config();
 mongoose.Promise = global.Promise;
 const cors = require("cors");
+const path= require('path');
 
 const app = express();
 
@@ -68,5 +69,15 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/auth", require("./routes/validator"));
 
-const PORT = process.env.PORT;
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT||8000;
 app.listen(PORT, console.log(`Server connected to port ${PORT}`));
